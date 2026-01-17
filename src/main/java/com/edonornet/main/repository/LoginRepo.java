ac8bc00159b6767f7b96fa7df9e1f1643da8eb0b
Yashwanth Varma <yashwanth.sse@gmail.com>
2026-01-17 23:17:23 +0530

    Phase-1 Done

--- src/main/java/com/edonornet/main/repository/LoginRepo.java
+++ src/main/java/com/edonornet/main/repository/LoginRepo.java
@@ -4,4 +4,5 @@ import com.edonornet.main.model.Login_info;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepo extends JpaRepository<Login_info, Long> {
+
 }
